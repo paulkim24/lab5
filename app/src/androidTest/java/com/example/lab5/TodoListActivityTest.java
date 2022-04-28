48eff1deba0423371dae7bc0d851aa006de8b487
Luis Cedeno <lcedeno@ucsd.edu>
2022-04-28 10:45:12 -0700

    new

--- app/src/androidTest/java/com/example/lab5/TodoListActivityTest.java
+++ app/src/androidTest/java/com/example/lab5/TodoListActivityTest.java
@@ -1,13 +1,20 @@
 package com.example.lab5;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import androidx.lifecycle.Lifecycle;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -38,6 +45,27 @@ public class TodoListActivityTest {
         todoListItemDao = testDb.todoListItemDao();
         todoListItemDao.insertAll(todos);
     }
+    @Test
+    public void testEditTodoText(){
+        String newText = "Ensure all tests pass";
+        ActivityScenario<TodoListActivity> scenario
+                = ActivityScenario.launch(TodoListActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+        scenario.moveToState(Lifecycle.State.RESUMED);
 
+        scenario.onActivity(activity -> {
+            List<TodoListItem> beforeTodoList = todoListItemDao.getAll();
 
+            EditText newTodoText = activity.findViewById(R.id.new_todo_text);
+            Button addTodoButton = activity.findViewById(R.id.add_todo_btn);
+
+            newTodoText.setText(newText);
+            addTodoButton.performClick();
+
+            List<TodoListItem> afterTodoList = todoListItemDao.getAll();
+            assertEquals(beforeTodoList.size() +1, afterTodoList.size());
+            assertEquals(newText, afterTodoList.get(afterTodoList.size()-1).text);
+        });
+    }
 }
