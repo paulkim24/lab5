b3154661f9a63e69d202ef8cf744083553720846
Paul Kim <pykim@ucsd.edu>
2022-04-28 11:30:35 -0700

    app

--- app/src/main/java/com/example/lab5/TodoListViewModel.java
+++ app/src/main/java/com/example/lab5/TodoListViewModel.java
@@ -1,4 +1,33 @@
 package com.example.lab5;
 
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class TodoListViewModel extends AndroidViewModel{
+    private LiveData<List<TodoListItem>> todoListItems;
+    private final TodoListItemDao todoListItemDao;
+
+    public TodoListViewModel(@NonNull Application application){
+        super(application);
+        Context context = getApplication().getApplicationContext();
+        TodoDatabase db = TodoDatabase.getSingleton(context);
+        todoListItemDao = db.todoListItemDao();
+    }
+
+    public LiveData<List<TodoListItem>> getTodoListItems(){
+        if (todoListItems == null) {
+            loadUsers();
+        }
+
+        return todoListItems;
+    }
+
+    private void loadUsers(){
+        todoListItems = todoListItemDao.getAllLive();
+    }
 }
